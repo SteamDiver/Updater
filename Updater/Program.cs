5936d80c0552a8c3b80596441c3deb093de6b347
SteamDiver <steamdiver@yandex.ru>
2017-11-04 02:02:16 +0300

    fix bugs

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Xml;
+using System.Net;
+using System.IO.Compression;
+using System.Windows.Forms;
 
 namespace Updater
 {
@@ -10,6 +16,60 @@ namespace Updater
     {
         static void Main(string[] args)
         {
+             Update(args[0]);
         }
+           
+           
+        static void Update(string programName)
+        {
+            try
+            {
+                string process = programName.Replace(".exe", "");
+                Console.WriteLine("Terminating process");
+                while (Process.GetProcessesByName(process).Length > 0)
+                {
+                    Process[] myProcesses2 = Process.GetProcessesByName(process);
+                    for (int i = 1; i < myProcesses2.Length; i++) { myProcesses2[i].Kill(); }
+
+                    Thread.Sleep(300);
+                }
+                Console.WriteLine("Success");
+                
+
+                DirectoryInfo directoryinfo = new DirectoryInfo("tmp");
+                if (directoryinfo.Exists)
+                {
+                    using (ZipArchive archive = ZipFile.OpenRead(@"tmp\update.zip"))
+                    {
+                        archive.ExtractToDirectory(Directory.GetCurrentDirectory());
+                        DeleteFromFile(@"_DELETE_.txt");
+                    }
+
+
+                    directoryinfo.Delete(true);
+                    Console.WriteLine("Success");
+                    Process.Start(programName + ".exe");
+                }
+
+            }
+            catch (Exception e) { Console.WriteLine(e.Message);}
+        }
+
+        static void DeleteFromFile(string filename)
+        {
+            FileInfo file = new FileInfo(filename);
+            if (file.Exists)
+            {
+                string[] filesToDelete = File.ReadAllLines(filename);
+                foreach (var delete in filesToDelete)
+                {
+                    FileInfo f = new FileInfo(delete);
+                    if (f.Exists) try { f.Delete(); } catch { };
+                }
+                file.Delete();
+            }
+            
+        }
+        
     }
 }
